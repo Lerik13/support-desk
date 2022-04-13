f41d63ea9721cf7f83cd4a155a98c21d5a91d295
Valeria <lerik13@gmail.com>
2022-04-13 11:44:27 -0600

    Hook Register form to Redux, Register User in client-side, save user-data in LocalStorage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import {useSelector, useDispatch} from 'react-redux'
+import {login} from '../features/auth/authSlice'
 
 function Login() {
 	const [formData, setFormData] = useState({
@@ -9,6 +11,10 @@ function Login() {
 	});
 
 	const {email, password} = formData
+
+	const dispatch = useDispatch()
+
+	const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
@@ -20,7 +26,12 @@ function Login() {
 	const onSubmit = (e) => {
 		e.preventDefault()
 
-		
+		const userData = {
+			email,
+			password
+		}
+
+		dispatch(login(userData))
 	}
 
 	return (
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,9 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import {FaUser} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import {useSelector, useDispatch} from 'react-redux'
+import { register, reset } from '../features/auth/authSlice'
 
 function Register() {
 	const [formData, setFormData] = useState({
@@ -11,6 +14,24 @@ function Register() {
 	});
 
 	const {name, email, password, password2} = formData
+
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
+
+	const {user, isLoading, isError, isSuccess, message} = useSelector(state => state.auth)
+
+	useEffect(() => {
+		if (isError) {
+			toast.error(message)
+		}
+
+		//Redirect when logged in
+		if (isSuccess || user) {
+			navigate('/')
+		}
+
+		dispatch(reset())
+	}, [isError, isSuccess, user, message, navigate, dispatch]);
 
 	const onChange = (e) => {
 		setFormData((prevState) => ({
@@ -24,9 +45,15 @@ function Register() {
 
 		if (password !== password2) {
 			toast.error('Passwords do not match')
-		}
+		} else {
+			const userData = {
+				name,
+				email,
+				password
+			}
 
-		
+			dispatch(register(userData))
+		}
 	}
 
 	return (
